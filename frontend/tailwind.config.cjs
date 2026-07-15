d6021331cc928c9255b62441203a99015db88a3c
ROLAND NUTIFAFA APENUVOR <apenuvor.nutifafa@gmail.com>
2026-07-15 14:52:03 +0000

    Create tailwind.config.cjs

---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: '#FFF9E6',
+          DEFAULT: '#1F2937',
+          dark: '#1F2937',
+          yellow: '#FBBF24',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
